remove batch normalization bug

computer_vision/projects/classification/tensorflow/simpleconv3/net.py
@@ -12,16 +12,16 @@ import tensorflow as tf
 
 debug=True
 
-def simpleconv3(x):
+def simpleconv3(x,istraining):
     x_shape = tf.shape(x)
     with tf.name_scope("simpleconv3"):
         with tf.variable_scope("conv3_net"):
             conv1 = tf.layers.conv2d(x, name="conv1", filters=12,kernel_size=[3,3], strides=(2,2), activation=tf.nn.relu,kernel_initializer=tf.contrib.layers.xavier_initializer(),bias_initializer=tf.contrib.layers.xavier_initializer())
-            bn1 = tf.layers.batch_normalization(conv1, training=True, name='bn1')
+            bn1 = tf.layers.batch_normalization(conv1, training=istraining, name='bn1')
             conv2 = tf.layers.conv2d(bn1, name="conv2", filters=24,kernel_size=[3,3], strides=(2,2), activation=tf.nn.relu,kernel_initializer=tf.contrib.layers.xavier_initializer(),bias_initializer=tf.contrib.layers.xavier_initializer())
-            bn2 = tf.layers.batch_normalization(conv2, training=True, name='bn2')
+            bn2 = tf.layers.batch_normalization(conv2, training=istraining, name='bn2')
             conv3 = tf.layers.conv2d(bn2, name="conv3", filters=48,kernel_size=[3,3], strides=(2,2), activation=tf.nn.relu,kernel_initializer=tf.contrib.layers.xavier_initializer(),bias_initializer=tf.contrib.layers.xavier_initializer())
-            bn3 = tf.layers.batch_normalization(conv3, training=True, name='bn3')
+            bn3 = tf.layers.batch_normalization(conv3, training=istraining, name='bn3')
             conv3_flat = tf.reshape(bn3, [-1, 5 * 5 * 48])
             dense = tf.layers.dense(inputs=conv3_flat, units=128, activation=tf.nn.relu,name="dense",kernel_initializer=tf.contrib.layers.xavier_initializer())
             logits= tf.layers.dense(inputs=dense, units=2, activation=tf.nn.relu,name="logits",kernel_initializer=tf.contrib.layers.xavier_initializer())

computer_vision/projects/classification/tensorflow/simpleconv3/train.py
@@ -27,7 +27,7 @@ if __name__=="__main__":
     iterator = dataset.data.make_one_shot_iterator()
     dataset_size = dataset.dataset_size
     batch_images,batch_labels = iterator.get_next()
-    Ylogits = simpleconv3(batch_images)
+    Ylogits = simpleconv3(batch_images,True)
 
     print "Ylogits size=",Ylogits.shape
 
@@ -39,7 +39,6 @@ if __name__=="__main__":
     update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
     with tf.control_dependencies(update_ops):
         train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
-
     saver = tf.train.Saver()
     in_steps = 100
     checkpoint_dir = 'checkpoints/'